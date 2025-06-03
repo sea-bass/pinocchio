bbfbd0b76deb7e89fb5c9e130ae277c488fdc337
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-06-03 17:29:12 +0200

    nix: use coal devel flake

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    coal-src = {
-      url = "github:coal-library/coal";
-      flake = false;
+    coal.url = "github:nim65s/coal/nix-warning";
+    coal.inputs = {
+      flake-parts.follows = "flake-parts";
+      nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -15,8 +16,18 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
-        { pkgs, self', ... }:
         {
+          inputs',
+          pkgs,
+          self',
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (_: _: { inherit (inputs'.coal.packages) coal; }) ];
+          };
           apps.default = {
             type = "app";
             program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
@@ -24,31 +35,25 @@
           devShells.default = pkgs.mkShell { inputsFrom = [ self'.packages.default ]; };
           packages = {
             default = self'.packages.pinocchio;
-            coal = pkgs.coal.overrideAttrs (super: {
-              src = inputs.coal-src;
-              cmakeFlags = super.cmakeFlags ++ [ "-DCOAL_DISABLE_HPP_FCL_WARNINGS=ON" ];
-            });
-            pinocchio =
-              (pkgs.pinocchio.overrideAttrs (super: {
-                src = pkgs.lib.fileset.toSource {
-                  root = ./.;
-                  fileset = pkgs.lib.fileset.unions [
-                    ./benchmark
-                    ./bindings
-                    ./CMakeLists.txt
-                    ./doc
-                    ./examples
-                    ./include
-                    ./models
-                    ./package.xml
-                    ./sources.cmake
-                    ./src
-                    ./unittest
-                    ./utils
-                  ];
-                };
-              })).override
-                { inherit (self'.packages) coal; };
+            pinocchio = pkgs.python3Packages.pinocchio.overrideAttrs {
+              src = pkgs.lib.fileset.toSource {
+                root = ./.;
+                fileset = pkgs.lib.fileset.unions [
+                  ./benchmark
+                  ./bindings
+                  ./CMakeLists.txt
+                  ./doc
+                  ./examples
+                  ./include
+                  ./models
+                  ./package.xml
+                  ./sources.cmake
+                  ./src
+                  ./unittest
+                  ./utils
+                ];
+              };
+            };
           };
         };
     };
