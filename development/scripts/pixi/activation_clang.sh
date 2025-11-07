6d850c2c66508744f212e0afadd57ec2987570e6
Joris Vaillant <joris.vaillant@inria.fr>
2025-11-07 15:26:46 +0100

    pixi: Remove unused scripts

--- development/scripts/pixi/activation_clang.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# Clang activation script
-
-export CC=clang
-export CXX=clang++
-
-# activation.sh set this variable to gcc, we must override it here
-export CCACHE_COMPILERTYPE=clang
